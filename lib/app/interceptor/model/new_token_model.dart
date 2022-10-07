92b2c4bbf722fe1dd440ae5edaab0aaa3c8f5def
VISHNU K <100349641+vshnu97@users.noreply.github.com>
2022-10-07 13:41:56 +0530

    Delete lib/app/interceptor directory

--- lib/app/interceptor/model/new_token_model.dart
+++
@@ -1,8 +0,0 @@
-class NewToken {
-  String token;
-  NewToken({required this.token});
-
-  factory NewToken.fromJSON(Map<String, dynamic> json) => NewToken(
-        token: json['token'],
-      );
-}
